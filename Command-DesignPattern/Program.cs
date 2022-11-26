acbf9ad0b7db18586edd8f2143d506f9301d6529
Novr_wz05 <Novr_wz05@STHQ0123-08>
2022-11-26 08:14:03 +0400

    Add project files.

---
+++ Command-DesignPattern/Program.cs
@@ -0,0 +1,40 @@
+﻿public interface ICommand
+{
+    public void Execute();
+}
+
+public class SaveButton : ICommand
+{
+    public void Execute()
+    {
+        Console.WriteLine("Project Saved!");
+    }
+}
+
+public class AddButton : ICommand
+{
+    public void Execute()
+    {
+        Console.WriteLine("Project Added!");
+    }
+}
+
+public class DeleteButton : ICommand
+{
+    public void Execute()
+    {
+        Console.WriteLine("Project Deleted!");
+    }
+}
+
+
+public class Button
+{
+    ICommand? _command;
+
+    public Button(ICommand command)
+    {
+        _command = command;
+    }
+}
+
